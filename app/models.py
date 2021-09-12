20f302f1b2ff149d95946983f5a7c3c5b689d37b
thuojose <joseph.thuo@student.moringaschool.com>
2021-09-12 21:04:13 +0300

    added model.py and class category

---
+++ app/models.py
@@ -0,0 +1,24 @@
+class Article:
+    '''
+    Class that instantiates objects of the news article objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
+
+class Category:
+    '''
+    Class that instantiates objects of the news categories objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
+
