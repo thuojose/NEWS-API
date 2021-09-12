ee7b7a5b418a5d1b8dc3d55ce4d27a723fdd20b0
thuojose <joseph.thuo@student.moringaschool.com>
2021-09-12 22:34:49 +0300

    added test and request.py for articles

--- app/request.py
+++ app/request.py
@@ -1,5 +1,5 @@
 import urllib.request,json
-from .models import Source
+from .models import Source,Article
 
 # Getting api key
 api_key = None
@@ -52,3 +52,38 @@ def process_results(source_list):
             source_results.append(source_object)
 
     return source_results
+
+def article_source(id):
+    article_source_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(id,api_key)
+    print(article_source_url)
+    with urllib.request.urlopen(article_source_url) as url:
+        article_source_data = url.read()
+        article_source_response = json.loads(article_source_data)
+
+        article_source_results = None
+
+        if article_source_response['articles']:
+            article_source_list = article_source_response['articles']
+            article_source_results = process_articles_results(article_source_list)
+
+
+    return article_source_results
+
+def process_articles_results(news):
+    '''
+    function that processes the json files of articles from the api key
+    '''
+    article_source_results = []
+    for article in news:
+        author = article.get('author')
+        description = article.get('description')
+        time = article.get('publishedAt')
+        url = article.get('urlToImage')
+        image = article.get('url')
+        title = article.get ('title')
+
+        if url:
+            article_objects = Article(author,description,time,image,url,title)
+            article_source_results.append(article_objects)
+
+    return article_source_results
